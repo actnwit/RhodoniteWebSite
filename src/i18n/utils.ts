816d50a5a37e1c969b4ee9cadafb5ca3143cf605
Yuki Shimada <emadurandal@gmail.com>
2025-05-18 15:19:06 +0900

    remove console.log

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -2,14 +2,9 @@ import { ui, defaultLang } from './ui';
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
-  console.log('URL pathname:', url.pathname);
-  console.log('Extracted lang:', lang);
-  console.log('Available langs:', Object.keys(ui));
   if (lang && lang in ui) {
-    console.log('Using lang:', lang);
     return lang as keyof typeof ui;
   }
-  console.log('Using default lang:', defaultLang);
   return defaultLang;
 }
 
@@ -17,4 +12,4 @@ export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
   }
-} 
+}
